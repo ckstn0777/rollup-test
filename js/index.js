ffba1ba2a723ad06b3354099b5ec5ead2ccadb46
ckstn0777 <ckstn0777@github.com>
2022-06-25 18:19:01 +0900

    rollup iife 방식으로 번들해서 라이브러리 사용

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import { sayHelloTo } from "./sayHello.js";
 import { sayGoodbyeTo } from "./sayGoodbye.js";
 
-console.log(sayHelloTo("ckstn"));
-//export { sayHelloTo, sayGoodbyeTo };
+//console.log(sayHelloTo("ckstn"));
+export { sayHelloTo, sayGoodbyeTo };
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
+// import resolve from "@rollup/plugin-node-resolve";
+
 export default {
-  input: "./src/index.js", // 진입 경로
+  input: "./js/index.js", // 진입 경로
   output: {
+    name: "utils",
     file: "./dist/bundle.js", // 출력 경로
-    format: "es", // 출력 형식
+    format: "iife", // 출력 형식
   },
+  // plugins: [resolve({ browser: true })],
 };
